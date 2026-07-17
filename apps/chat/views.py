b6651e37e6d0d5948ba1a20eaadee251255f7fea
Adityagupta-18 <aditya.work018@gmail.com>
2026-07-17 22:33:26 +0530

    completed new chat logic

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -6,15 +6,33 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 def new_chat(request):
     if request.method=='POST':
-        conversation=Conversation.objects.create(
-            user=request.user,
-            title="New Chat")
-        return JsonResponse({
+        new_chats = Conversation.objects.filter(user=request.user,title="New Chat")
+        for conversation in new_chats:
+            if not conversation.messages.exists():
+                return JsonResponse({
                 "success": True,
                 "id": conversation.id,
                 "title": conversation.title})
+        
+        conversation = Conversation.objects.create(
+            user=request.user,
+            title="New Chat"
+        )
+        return JsonResponse({
+            "success": True,
+            "conversation_id": conversation.id,
+            "title": conversation.title
+        })
+            
+    return JsonResponse({
+            "success": False,
+            "message": "Invalid request"}, status=400)
     
 def conversation_detail(request,conversation_id):
-    conversation=get_object_or_404(Conversation,id=conversation_id)
-    mesg=conversation.messages.all().order_by('created_at')
-    return render(request,"base.html",{'conversation':conversation , 'message':mesg})
+    user=request.user
+    all_conversations=Conversation.objects.filter(user=user).order_by('-updated_at')
+    conversation=get_object_or_404(Conversation,id=conversation_id,user=user)
+    messages=conversation.messages.all().order_by('created_at')
+
+    context={'conversation':conversation , 'messages':messages , 'all_conversations':all_conversations}
+    return render(request,"core/home.html",context)
--- static/js/main.js
+++ static/js/main.js
@@ -3,24 +3,24 @@ const csrfToken = document.querySelector(
     "[name=csrfmiddlewaretoken]"
 ).value;
 
-function addConversationToSidebar(data){
-    const todaygroup=document.getElementById("today-group");
-    const newConversation = document.createElement("a");
-    const activeConversations = document.querySelectorAll(".sidebar-conversation.active");
-    activeConversations.forEach(conversation => {
-        conversation.classList.remove("active")});
-    newConversation.innerHTML=`<svg class="icon icon-sm" aria-hidden="true"><use href="#i-chat"></use></svg>
-                                <span class="sidebar-conversation-title">${data.title}</span>
-                                <svg class="icon icon-sm sidebar-conversation-more" aria-hidden="true"><use href="#i-dots"></use></svg>`;
-                                newConversation.classList.add('sidebar-conversation')
-                                newConversation.classList.add('active')
-                                newConversation.href='#'
-                                newConversation.dataset.conversationId = data.conversation_id;
+// function addConversationToSidebar(data){
+//     const todaygroup=document.getElementById("today-group");
+//     const newConversation = document.createElement("a");
+//     const activeConversations = document.querySelectorAll(".sidebar-conversation.active");
+//     activeConversations.forEach(conversation => {
+//         conversation.classList.remove("active")});
+//     newConversation.innerHTML=`<svg class="icon icon-sm" aria-hidden="true"><use href="#i-chat"></use></svg>
+//                                 <span class="sidebar-conversation-title">${data.title}</span>
+//                                 <svg class="icon icon-sm sidebar-conversation-more" aria-hidden="true"><use href="#i-dots"></use></svg>`;
+//                                 newConversation.classList.add('sidebar-conversation')
+//                                 newConversation.classList.add('active')
+//                                 newConversation.href='#'
+//                                 newConversation.dataset.conversationId = data.id;
 
-    const label = document.querySelector(".sidebar-group-label");
-    label.insertAdjacentElement("afterend", newConversation);
+//     const label = document.querySelector(".sidebar-group-label");
+//     label.insertAdjacentElement("afterend", newConversation);
 
-}
+// }
 
 const Newchatbutton=document.getElementById("new-chat-btn")
 Newchatbutton.addEventListener('click',()=>{
@@ -29,5 +29,7 @@ Newchatbutton.addEventListener('click',()=>{
      headers: {
         "X-CSRFToken": csrfToken
     }}).then(response=>response.json())
-    .then(data=>{addConversationToSidebar(data)})
+    .then(data=>{
+        window.location.href=`/chat/${data.id}/`
+    })
 })

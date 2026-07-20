1d011e517020807472a11d7ae45bd1f63edd1dd4
Adityagupta-18 <aditya.work018@gmail.com>
2026-07-20 14:40:06 +0530

    added all message  history

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -48,16 +48,17 @@ def send_message(request):
         conversation_id = data.get("conversation_id")
         content = data.get("content", "").strip()
 
-        conversation = get_object_or_404(
-            Conversation,
-            id=conversation_id,
-            user=request.user)
+        conversation = get_object_or_404(Conversation,id=conversation_id,user=request.user)
 
-        Message.objects.create(
-            conversation=conversation,
-            sender="USER",
-            content=content
-        )
+        Message.objects.create(conversation=conversation,sender="USER",content=content)
+        history = conversation.messages.all().order_by("created_at")
+        history_dict=[]
+        for mesgcont in history:
+            if mesgcont.sender=='USER':
+                history_dict.append({"role":"user","content":mesgcont.content})
+            else:
+                history_dict.append({"role":"assistant","content":mesgcont.content})
+        print(history_dict)
         return JsonResponse({
             'success':True,
             "message": "Message saved successfully"
